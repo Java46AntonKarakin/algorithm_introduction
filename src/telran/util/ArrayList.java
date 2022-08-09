e01a4e1de8bfeada9123505645e2b6812116e6aa
volnykot <volnykot3@gmail.com>
2022-08-09 12:15:12 +0300

    adds HW#13

--- src/telran/util/ArrayList.java
+++ src/telran/util/ArrayList.java
@@ -2,106 +2,158 @@ package telran.util;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.function.Predicate;
+
 public class ArrayList<T> implements List<T> {
-private static final int DEFAULT_CAPACITY = 16;
-private T[] array;
-private int size;
-@SuppressWarnings("unchecked")
-public ArrayList(int capacity) {
-	array = (T[]) new Object[capacity];
-}
-public ArrayList() {
-	this(DEFAULT_CAPACITY);
-}
-private class ArrayListIterator implements Iterator<T> {
+	private static final int DEFAULT_CAPACITY = 16;
+	private T[] array;
+	private int size;
 
-	@Override
-	public boolean hasNext() {
-		// TODO Auto-generated method stub
-		return false;
+	@SuppressWarnings("unchecked")
+	public ArrayList(int capacity) {
+		array = (T[]) new Object[capacity];
 	}
 
-	@Override
-	public T next() {
-		// TODO Auto-generated method stub
-		return null;
+	public ArrayList() {
+		this(DEFAULT_CAPACITY);
 	}
-	
-}
+
+	private class ArrayListIterator implements Iterator<T> {
+		int currentIndex = 0;
+
+		@Override
+		public boolean hasNext() {
+			return currentIndex < size;
+		}
+
+		@Override
+		public T next() {
+			if (!hasNext()) {
+				throw new NoSuchElementException();
+			}
+			return array[currentIndex++];
+		}
+	}
+
 	@Override
 	public boolean add(T obj) {
 		if (array.length == size) {
 			array = Arrays.copyOf(array, size * 2);
 		}
 		array[size++] = obj;
+
 		return true;
 	}
 
 	@Override
 	public boolean remove(Object pattern) {
-		// TODO Auto-generated method stub
-		//array reallocation isn't done
-		//that is new array won't be created - essence of remove
-		//to use System.arraycopy
-		// size--
-		return false;
+		boolean flIsRemoved = false;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] != null && array[i].equals(pattern)) {
+				deleteElementByIndex(i);
+				flIsRemoved = true;
+				break;
+			}
+		}
+		return flIsRemoved;
 	}
 
 	@Override
 	public boolean removeIf(Predicate<T> predicate) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean flIsRemoved = false;
+		int index = 0;
+		while (index != size && array[index] != null) {
+			if (predicate.test(array[index])) {
+				deleteElementByIndex(index);
+				flIsRemoved = true;
+			} else {
+				index++;
+			}
+		}
+		return flIsRemoved;
 	}
 
 	@Override
 	public boolean contains(Object pattern) {
-		// TODO Auto-generated method stub
-		return false;
+
+		return indexOf(pattern) >= 0;
 	}
 
 	@Override
 	public int size() {
-		
+
 		return size;
 	}
 
 	@Override
 	public Iterator<T> iterator() {
-		
+
 		return new ArrayListIterator();
 	}
 
 	@Override
 	public boolean add(int index, T obj) {
-		// TODO Auto-generated method stub
-		//if size == array.length you should do reallocation see the method add
-				//if size < array.length new array won't be created - essence of the algorithm
-		return false;
+		if (index < 0 || index > size+1) {
+			return false;
+		}
+
+		if (size == array.length) {
+			array = Arrays.copyOf(array, size * 2);
+		}
+		System.arraycopy(array, index, array, index + 1, size++ - index);
+		array[index] = obj;
+		return true;
 	}
 
 	@Override
 	public T remove(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		if (index < 0 || index > size) {
+			return null;
+		}
+		
+		T res = array[index];
+		if (index == 0) {
+			System.arraycopy(array, index + 1, array, index, size-- - index);
+		} else if (index == size) {
+			array[index] = null;
+		} else {
+			System.arraycopy(array, index, array, index, size-- - index);
+		}
+		
+		return res;
 	}
 
 	@Override
 	public int indexOf(Object pattern) {
-		// TODO Auto-generated method stub
-		return 0;
+		int indexCounter = 0;
+		for (T obj : array) {
+			if (pattern.equals(obj)) {
+				return indexCounter;
+			}
+			indexCounter++;
+		}
+		return -1;
 	}
 
 	@Override
 	public int lastIndexOf(Object pattern) {
-		// TODO Auto-generated method stub
-		
-		return 0;
-	}
-	@Override
-	public T get(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		for (int i = size; i >= 0; i--) {
+			if (pattern.equals(array[i])) {
+				return i;
+			}
+		}
+		return -1;
 	}
 
+	@Override
+	public T get(int index) {
+		return (index >= 0 && index < size) ? array[index] : null;
+	}
+
+	private T[] deleteElementByIndex(int index) {
+		System.arraycopy(array, index + 1, array, index, size-- - index);
+		array[size] = null;
+		return array;
+	}
 }
--- src/telran/util/Collection.java
+++ src/telran/util/Collection.java
@@ -1,29 +1,39 @@
 package telran.util;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.function.Predicate;
 
 public interface Collection<T> extends Iterable<T> {
-/**
- * adds object of type T in collection
- * @param obj
- * @return true if added
- */
-	boolean add(T obj);
+	/**
+	 * adds object of type T in collection
+	 * 
+	 * @param obj
+	 * @return true if added
+	 */
+	default boolean add(T obj) {
+
+		return true;
+	}
+
 	/***************************************/
-	/** 
-	 * removes object equaled to the given pattern 
+	/**
+	 * removes object equaled to the given pattern
+	 * 
 	 * @param pattern any object
-	 * @return true if removed 
+	 * @return true if removed
 	 */
 	boolean remove(Object pattern);
+
 	/******************************************/
 	/**
 	 * removes all objects matching the given predicate
+	 * 
 	 * @param predicate
 	 * @return true if a collection has been updated
 	 */
 	boolean removeIf(Predicate<T> predicate);
+
 	/*************************************************/
 	/**
 	 * 
@@ -31,12 +41,14 @@ public interface Collection<T> extends Iterable<T> {
 	 * @return true if there is an object equaled to the given pattern
 	 */
 	boolean contains(Object pattern);
+
 	/********************************************************/
 	/**
 	 * 
 	 * @return amount of the objects
 	 */
 	int size();
+
 	/******************************************************/
 	/**
 	 * 
@@ -44,14 +56,34 @@ public interface Collection<T> extends Iterable<T> {
 	 * @return regular Java array containing all the collection object
 	 */
 	default T[] toArray(T[] ar) {
-		//TODO
-		// write the default method implementation based on the iterating
 		Iterator<T> it = iterator();
-		//TODO fill array by iterating 
-		//if ar.length < size then you should create new array of type T with proper length(consider method Arrays.copyOf)
-		//if ar.length == size then you just fill the given array and reference to the same array will be returned
-		//if ar'length > size then you fill the given array and rest part should be filled by null's and 
-		// reference to the same array will be returned
-		return null;
+		int size = size();
+		int current = 0;
+		
+		
+		if (ar.length < size) {
+			ar = Arrays.copyOf(ar, size);
+		} else if (ar.length > size) {
+			for (int i = size; i < ar.length; i++) {
+				ar[i] = null;
+			}
+		}
+		
+//		if (ar.length < size) {
+//			ar = Arrays.copyOf(ar, size);
+//		}
+
+		while (it.hasNext()) {
+			ar[current++] = it.next();
+		}
+
+//		if (ar.length > size) {
+//			int fillNullFromIndex = size - ar.length;
+//			for (; fillNullFromIndex < size - 1; fillNullFromIndex++) {
+//				ar[fillNullFromIndex] = null;
+//			}
+//		}
+		return ar;
 	}
+	
 }
